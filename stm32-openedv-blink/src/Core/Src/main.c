8d5aa294829f42c09a1a50bbbd2fc856ced2c2b9
12157724@qq.com <12157724@qq.com>
2023-11-20 14:33:58 +0800

    beep key

--- stm32-openedv-blink/src/Core/Src/main.c
+++ stm32-openedv-blink/src/Core/Src/main.c
@@ -21,24 +21,47 @@
 #include "thesys.h"
 #include "delay.h"
 #include "usart.h"
-
+#include "beep.h"
+#include "key.h"
 
 
 int main(void)
 {
-    HAL_Init();                                 /* 初始化HAL库 */
-    sys_stm32_clock_init(RCC_PLL_MUL9);         /* 设置时钟,72M */
-    delay_init(72);                             /* 初始化延时函数 */
-    led_init();                                 /* 初始化LED */
-    
-    while(1)
+    uint8_t key;
+
+    HAL_Init();                         /* 初始化HAL库 */
+    sys_stm32_clock_init(RCC_PLL_MUL9); /* 设置时钟, 72Mhz */
+    delay_init(72);                     /* 延时初始化 */
+    led_init();                         /* 初始化LED */
+    beep_init();                        /* 初始化蜂鸣器 */
+    key_init();                         /* 初始化按键 */
+    LED0(0);                            /* 先点亮LED0 */
+
+    while (1)
     {
-        LED0(0);                                /* LED0 亮 */
-        LED1(1);                                /* LED1 灭 */
-        delay_ms(500);
-        LED0(1);                                /* LED0 灭 */
-        LED1(0);                                /* LED1 亮 */
-        delay_ms(500);
+        key = key_scan(0); /* 得到键值 */
+
+        if (key)
+        {
+            switch (key)
+            {
+            case WKUP_PRES:    /* 控制蜂鸣器 */
+                BEEP_TOGGLE(); /* BEEP状态取反 */
+                break;
+
+            case KEY1_PRES:    /* 控制LED1(GREEN)翻转 */
+                LED1_TOGGLE(); /* LED1状态取反 */
+                break;
+
+            case KEY0_PRES:    /* 同时控制LED0, LED1翻转 */
+                LED0_TOGGLE(); /* LED0状态取反 */
+                LED1_TOGGLE(); /* LED1状态取反 */
+                break;
+            }
+        }
+        else
+        {
+            delay_ms(10);
+        }
     }
 }
-
